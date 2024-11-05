91b613826e85ec83ab7a39809047d2bbabb63ce0
NgocSon-AI <286sunsun@gmail.com>
2024-11-05 20:46:30 +0700

    code skelaton updated

---
+++ Xray/cloud_storage/s3_operation.py
@@ -0,0 +1,18 @@
+import os
+import sys
+from Xray.exception import XRayException
+
+
+class S30peration():
+    def sync_folder_to_s3(self, ) -> None:
+        try:
+            pass
+        except Exception as e:
+            raise XRayException(e, sys)
+    
+    def sync_folder_to_s3(folder)->None:
+        try:
+            pass
+        except Exception as e:
+            raise XRayException(e, sys)
+     
